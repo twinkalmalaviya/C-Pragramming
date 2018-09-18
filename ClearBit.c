0c0e292e7ad0620808650cd25c98987163f06f15
twinkal <twinkalmalaviya91@gmail.com>
2018-09-18 16:40:54 +0530

    Create ClearBit.c
    function to clear Nth position of bit in given number

---
+++ ClearBit.c
@@ -0,0 +1,14 @@
+	
+#include<stdio.h>
+/*function to clear given number of bit*/
+ int Clear_bit(int n,int number_of_b)
+ {
+   return(n & (~(1<<(number_of_b-1))));
+ }
+int main()
+{
+  int num =9,number_of_bit=1;
+  printf("befor bit clear=%d\n",num);
+  printf("after bit clear=%d\n",Clear_bit(num,number_of_bit));
+ 
+}
