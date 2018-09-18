f841288894046f8b02e751eaf676e5b134eade8b
twinkal <twinkalmalaviya91@gmail.com>
2018-09-18 15:57:55 +0530

    Update Startup.c

--- Startup.c
+++ Startup.c
@@ -2,18 +2,18 @@
 #include<stdio.h>
  
 /* Apply the constructor attribute to StartupFunction() so that it is executed before main() */
-void StartupFunction(void) __attribute__ ((constructor));
+void StartupFun(void) __attribute__ ((constructor));
 /* Apply the destructor attribute to CleanupFunction() so that it is executed after main() */
-void CleanupFunction(void) __attribute__ ((destructor));
+void CleanupFun(void) __attribute__ ((destructor));
 /* implementation of StartupFunction*/
-void StartupFunction(void)
+void StartupFun(void)
 {
-    printf ("startup code before main()\n");
+    printf ("startup code\n");
 }
 /* implementation of CleanupFunctiom */
-void CleanupFunction(void)
+void CleanupFun(void)
 {
-    printf ("cleanup code after main()\n");
+    printf ("startup code\n");
 }
  
 int main (void)
