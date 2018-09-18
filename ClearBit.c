6bfe7cf6687932d511df0c2c3b1b2cbea2fbce0d
twinkal <twinkalmalaviya91@gmail.com>
2018-09-18 16:51:33 +0530

    Update ClearBit.c

--- ClearBit.c
+++ ClearBit.c
@@ -3,11 +3,14 @@
 /*function to clear given number of bit*/
  int Clear_bit(int n,int number_of_b)
  {
+   if(number_of_b)
    return(n & (~(1<<(number_of_b-1))));
+   else
+   return(n);
  }
 int main()
 {
-  int num =9,number_of_bit=1;
+  int num =1,number_of_bit=1;
   printf("befor bit clear=%d\n",num);
   printf("after bit clear=%d\n",Clear_bit(num,number_of_bit));
  
