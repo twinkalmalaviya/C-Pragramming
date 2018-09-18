285796039de1a19c98a182823a67a7f4c0ad1203
twinkal <twinkalmalaviya91@gmail.com>
2018-09-18 14:58:54 +0530

    Update swap_code.c

--- swap_code.c
+++ swap_code.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  int num1=5,num2=9;
-  printf("number before swap\n num1= %d,num2=%d\n",num1,num2);
+  int num1=0,num2=9;
+  printf("number before swap\nnum1= %d,num2=%d\n",num1,num2);
+  /*method one */
 /* swap two number wihtout temp */
   num1=num1+num2;
   num2=num1-num2;
   num1=num1-num2;
-  printf("number after swap\n num1= %d,num2=%d\n",num1,num2);
+  printf("number after swap\nnum1= %d,num2=%d\n",num1,num2);
+  /*method two */
+  num1= num1+num2-(num2=num1);
+    printf("number after swap\nnum1= %d,num2=%d\n",num1,num2);
 
 }
